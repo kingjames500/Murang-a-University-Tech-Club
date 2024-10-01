363fa6e0e59b007e13618fc69c739897e568a5d9
kingjames500 <jimmymaina500@gmail.com>
2024-10-01 21:59:14 +0300

    downgrading from react@18 to react@17.0.2

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,28 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
+// react 17.0.2
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './App.css';
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>,
+  document.getElementById('root')
 );
+
+
+
+
+// react 18 which I have downgrade to 17.0.2
+// import { StrictMode } from "react";
+// import { createRoot } from "react-dom/client";
+// import App from "./App.jsx";
+
+// createRoot(document.getElementById("root")).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// );
